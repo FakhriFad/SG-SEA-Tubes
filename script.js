4cd6b130caf283deb4ec4aec5205d79ed8a16c44
FakhriFad <fakhfad@gmail.com>
2023-12-03 23:56:49 +0700

    Change sidenav

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+var delay = 500;
+
 function toggleNav() {
     const element = document.getElementById("sideNav");
+    const element1 = document.getElementById("navLinks");
     if (element.className == "closeNav") {
         element.className = "openNav";
+        setTimeout(function() {
+            element1.className = "navLinks-open";
+        }, delay);
     } else {
         element.className = "closeNav";
+        element1.className = "navLinks-close";
     }
 }
