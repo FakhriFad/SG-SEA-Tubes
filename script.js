54a716f67da3b32c9e2e3045e3a2a11d8aec0921
FakhriFad <fakhfad@gmail.com>
2023-12-04 13:50:01 +0700

    Tidying up sidebar, scrap highlight scroll idea

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 var delay = 500;
 
 function toggleNav() {
-    const element = document.getElementById("sideNav");
-    const element1 = document.getElementById("navLinks");
+    const element = document.getElementById("nav");
     if (element.className == "closeNav") {
         element.className = "openNav";
-        /*setTimeout(function() {
-            element1.className = "navLinks-open";
-        }, delay);*/
     } else {
         element.className = "closeNav";
-        //element1.className = "navLinks-close";
     }
 }
